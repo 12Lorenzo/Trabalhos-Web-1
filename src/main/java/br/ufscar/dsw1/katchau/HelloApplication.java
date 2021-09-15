927eb3797ff43f8fc82fdb33dac8d6f9662358b5
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-09-14 21:08:30 -0300

    Commit inicial
    Co-Authored-By: Lorenzo Correia Maia <17934954+12Lorenzo@users.noreply.github.com>

---
+++ src/main/java/br/ufscar/dsw1/katchau/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.ufscar.dsw1.katchau;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
