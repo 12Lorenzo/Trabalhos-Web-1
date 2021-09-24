96035b4d64278203925242cb0d2ce569af20afd8
12Lorenzo <lorenzocbm@gmail.com>
2021-09-24 00:28:25 -0300

    Fazendo
    Co-authored-by: OakBranches <matheusramosimperador@gmail.com>

--- src/main/java/br/ufscar/dsw1/katchau/controle/CarroController.java
+++ src/main/java/br/ufscar/dsw1/katchau/controle/CarroController.java
@@ -4,6 +4,9 @@ package br.ufscar.dsw1.katchau.controle;
 
 import br.ufscar.dsw1.katchau.dao.CarroDAO;
 import br.ufscar.dsw1.katchau.entidade.*;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,12 +14,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static br.ufscar.dsw1.katchau.Constants.*;
+import static br.ufscar.dsw1.katchau.Constants.UPLOAD_DIRECTORY;
 
 @WebServlet(name="Carros", urlPatterns = "/carros/*")
 public class CarroController extends HttpServlet {
@@ -63,6 +70,46 @@ public class CarroController extends HttpServlet {
         criar(req, resp);
     }
 
+    protected void mandarmagem(HttpServletRequest request, HttpServletResponse response, long id) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+
+        if (ServletFileUpload.isMultipartContent(request)) {
+
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            factory.setSizeThreshold(MEMORY_THRESHOLD);
+            factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            upload.setFileSizeMax(MAX_FILE_SIZE);
+            upload.setSizeMax(MAX_REQUEST_SIZE);
+            String localPath = UPLOAD_DIRECTORY + File.separator + id;
+            String uploadPath = getServletContext().getRealPath(localPath);
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            try {
+                List<FileItem> formItems = upload.parseRequest(request);
+                if (formItems != null && formItems.size() > 0) {
+                    for (FileItem item : formItems) {
+                        if (!item.isFormField()) {
+                            String fileName = new File(item.getName()).getName();
+                            String filePath = uploadPath + File.separator + fileName;
+                            File storeFile = new File(filePath);
+                            item.write(storeFile);
+                            request.getSession().setAttribute("message", "File " + fileName + " has uploaded successfully!");
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                request.getSession().setAttribute("message", "There was an error: " + ex.getMessage());
+            }
+            response.sendRedirect(request.getContextPath());
+        }
+    }
+
     private void criar (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/adm/formCarro.jsp");
         //dispatcher.forward(request, response);
@@ -96,7 +143,7 @@ public class CarroController extends HttpServlet {
                 }
 
                 //todo upload das imagens
-
+                mandarmagem(request, response, id_carro);
 
 
                 System.out.println("Entrou para dar insert " + id_carro);
